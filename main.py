3f9f7e977e22e17e72d3a948c2bbace359a5a8f9
cat <catcampbellc@gmail.com>
2025-11-24 21:55:50 -0800

    Add sorted charcter report to Bookbot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_chars_dict
+from stats import get_num_words, get_chars_dict, character_sorted_list
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -10,8 +10,21 @@ def main():
     text = get_book_text(book_path)
     word_count = get_num_words(text)
     chars_dict = get_chars_dict(text)
+    sorted_chars = character_sorted_list(chars_dict)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
-    print(chars_dict)
+    print("--------- Character Count -------")
+
+    for item in sorted_chars:
+        ch = item["char"]
+        count = item["num"]
+        if ch.isalpha():
+            print(f"{ch}: {count}")
+
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,13 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
+
+def sort_on(item):
+    return item["num"]
+
+def character_sorted_list(chars_dict):
+    character_sorted_list = []
+    for ch in chars_dict:
+        character_sorted_list.append({"char": ch, "num": chars_dict[ch]})
+    character_sorted_list.sort(reverse=True, key=sort_on)
+    return character_sorted_list
